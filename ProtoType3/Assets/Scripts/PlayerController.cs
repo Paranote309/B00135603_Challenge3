0cb5b8aad910d3d31859d5998123f435f1b942ce
ParaNote <78971278+Paranote309@users.noreply.github.com>
2021-10-30 21:45:59 +0100

    3.2.4

--- ProtoType3/Assets/Scripts/PlayerController.cs
+++ ProtoType3/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 5;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -26,6 +27,12 @@ public class PlayerController : MonoBehaviour
 
     }
     private void onCollisonEnter(Collision collision){
+        if(collision.gameObject.CompareTag("Ground")){
             isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle")){
+            gameOver = true;
+            Debug.Log("Game over!");
+        }
     }
 }
