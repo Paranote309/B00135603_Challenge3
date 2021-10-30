8de79cbe47e7997eeb59a4c3b7aa9820947d4e92
ParaNote <78971278+Paranote309@users.noreply.github.com>
2021-10-30 22:07:40 +0100

    move left errors fixed

--- ProtoType3/Assets/Challenge 3/Scripts/MoveLeftX.cs
+++ ProtoType3/Assets/Challenge 3/Scripts/MoveLeftX.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class MoveLeftX : MonoBehaviour
 {
-    public float speed;
+    public float speed = 10;
     private PlayerControllerX playerControllerScript;
-    private float leftBound = -10;
+    private float leftBound = -11;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+        // playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // If game is not over, move to the left
-        if (playerControllerScript.gameOver)
+        if (playerControllerScript.gameOver == false)
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
         }
